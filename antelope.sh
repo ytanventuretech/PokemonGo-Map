4f9c248792058ac4fb3c3dcab8c5a2f9db366bdc
Ying Tan <ytan@i2rd.com>
2016-07-23 06:37:52 -0500

    Remove gym and pokestop

--- antelope.sh
+++ antelope.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-python runserver.py -a google -u ytanpokemon@gmail.com -p pokemonmap -l "40.7965033, -96.6735399" -st 10 -dp -dg
+python runserver.py -a google -u ytanpokemon@gmail.com -p pokemonmap -l "40.7965033, -96.6735399" -st 10
