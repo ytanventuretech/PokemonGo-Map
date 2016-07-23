01d5ac0b0db94dfc64403e3b3e374f49d7c4dc99
Ying Tan <ytan@i2rd.com>
2016-07-23 06:38:39 -0500

    Remove gym and pokestop

--- home.sh
+++ home.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-python runserver.py -a google -u ytanpokemon@gmail.com -p pokemonmap -l "40.737792, -96.671943" -st 10 -dp -dg
+python runserver.py -a google -u ytanpokemon@gmail.com -p pokemonmap -l "40.737792, -96.671943" -st 10
