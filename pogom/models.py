90bfd6ef75d8b8dd6472aca574b6b46bdd986ef8
Ying Tan <ytan@venturetech.net>
2016-07-24 11:51:28 -0500

    Refactor the code and add local time.

--- pogom/models.py
+++ pogom/models.py
@@ -6,36 +6,22 @@ from peewee import Model, SqliteDatabase, InsertQuery, IntegerField, \
     CharField, FloatField, BooleanField, DateTimeField
 from datetime import datetime
 from base64 import b64encode
-
 from .utils import get_pokemon_name
-from pogom.utils import get_args
+from pogom.utils import get_args, load_profile, send_email, shurl
 from pogom.pgoapi.utilities import get_pos_by_name
-import smtplib
-from sys import argv
-import httplib2
-import simplejson as json
+from dateutil import tz
 
 db = SqliteDatabase('pogom.db')
 log = logging.getLogger(__name__)
 
-interested_pokegroup = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 
-         '23', '24', '25', '26', '27', '28', '30', '31',
-         '33', '34', '35', '36', '37', '38', '39', '40', 
-         '50', '51', '53', '57', '58', '59', '61', '62', 
-         '63', '64', '65', '67', '68', '72', '73', '74', 
-         '75', '76', '77', '78', '81', '82', '83', '84', 
-         '85', '87', '89', '91', '93', '94', '95', '100',
-         '101', '103', '105', '106', '107', '108', '110',
-         '111', '113', '114', '115', '117', '119', '122',
-         '124', '125', '126', '127', '128', '129', '130', 
-         '131', '132', '137', '138', '139', '140', '141', '142', '143',
-         '144', '145', '146', '147', '148', '149', '150', '151']
+POKE_GROUP = load_profile()[1]
+EMAIL_TO = load_profile()[0]
 SENT = []
-TXT_TO = ['3085391356@vtext.com',
-          '4024692675@vtext.com'
-          #,'4024176691@messaging.sprintpcs.com'
-         ]
-EMAIL_TO = ['yingtan81@gmail.com', 'zhmanthony@gmail.com', 'crocole@gmail.com', 'toddraychrisman@gmail.com', 'chengqian.ty@gmail.com']
+position = get_pos_by_name(get_args().location)
+map_center = str(position[0]) + ',' + str(position[1])
+username = get_args().username
+password = get_args().password
+local_timezone = tz.tzlocal()
 
 
 class BaseModel(Model):
@@ -154,86 +140,58 @@ def parse_map(map_dict):
     if pokemons:
         log.info("Upserting {} pokemon".format(len(pokemons)))
         InsertQuery(Pokemon, rows=pokemons.values()).upsert().execute()
-        args = get_args()
-        position = get_pos_by_name(args.location)
-        office = str(position[0]) + ',' + str(position[1])
-        username = str(args.username)
-        password = str(args.password)
 
-        for p in pokemons.values():
-            # log.info(p)
-            p['pokemon_name'] = get_pokemon_name(p['pokemon_id'])
-            pokemon_name = p['pokemon_name'].lower()
-            pokemon_id = str(p['pokemon_id'])
-            if pokemon_id in interested_pokegroup and p['encounter_id'] not in SENT:
-                SENT.append(p['encounter_id'])
-                to = 'PokemonFan'
-                loc = str(p['latitude']) + ',' + str(p['longitude'])
-                icon = 'http://media.pldh.net/pokexycons/' + pokemon_id.zfill(3) + '.png'
-                pokeMsg = pokemon_name + ' will disappear at ' + p['disappear_time'].strftime('%X') + '\n'
-                img_url = 'https://maps.googleapis.com/maps/api/staticmap' \
-                      '?center=' + office + ')}' \
-                      '&zoom=15&size=640x640&markers=icon:' \
-                      + icon.encode('utf-8').strip() + '%7C' \
-                      + loc + '&key=AIzaSyDn-kxyG5NrrpFSft95w30SWR3YETJ5xDU'
+        if len(EMAIL_TO) > 0 and len(POKE_GROUP) > 0:
+            for p in pokemons.values():
+                # log.info(p)
+                p['pokemon_name'] = get_pokemon_name(p['pokemon_id'])
+                pokemon_name = p['pokemon_name'].lower()
+                pokemon_id = str(p['pokemon_id'])
 
-                short_url = shurl(img_url)
-                url = ''
-                if short_url:
-                    url = short_url
-                else:
-                    url = img_url
+                if pokemon_id in POKE_GROUP and p['encounter_id'] not in SENT:
+                    SENT.append(p['encounter_id'])
 
-                message = "\r\n".join([
-                    "From: %s" % username,
-                    "To: %s" % to,
-                    "Subject: %s" % pokemon_name,
-                    "",
-                    pokeMsg + url
-                ])
+                    if len(SENT) > 10000:
+                        del SENT[0:1000]
 
-                log.info("Send TXT: " + message.encode('utf-8').strip())
+                    loc = str(p['latitude']) + ',' + str(p['longitude'])
+                    icon = 'http://media.pldh.net/pokexycons/' + pokemon_id.zfill(3) + '.png'
+                    disappear_time = p['disappear_time'].replace(tzinfo=local_timezone)
+                    short_msg = pokemon_name + ' will disappear at ' + disappear_time.strftime('%X') + '\n'
+                    img_url = 'https://maps.googleapis.com/maps/api/staticmap' \
+                              '?center=' + map_center + ')}' \
+                                                        '&zoom=15&size=640x640&markers=icon:' \
+                              + icon.encode('utf-8').strip() + '%7C' \
+                              + loc + '&key=AIzaSyDn-kxyG5NrrpFSft95w30SWR3YETJ5xDU'
 
-                server = smtplib.SMTP("smtp.gmail.com", 587)
-                server.ehlo()
-                server.starttls()
-                server.login(username, password)
-                server.sendmail(username, EMAIL_TO, message.encode('utf-8').strip())
-                # for to in TXT_TO:
-                #     server.sendmail(username, to, message.encode('utf-8').strip())
-                server.quit()
+                    short_url = shurl(img_url)
+                    if short_url:
+                        url = short_url
+                    else:
+                        url = img_url
+
+                    message = "\r\n".join([
+                        "From: %s" % username,
+                        "To: %s" % 'PokemonFan',
+                        "Subject: %s" % pokemon_name,
+                        "",
+                        short_msg + url
+                    ])
+
+                    log.info("Send TXT: " + message)
+                    send_email(username, password, EMAIL_TO, message)
+
 
     # if pokestops:
     #    log.info("Upserting {} pokestops".format(len(pokestops)))
     #    InsertQuery(Pokestop, rows=pokestops.values()).upsert().execute()
 
-    if gyms:
-        log.info("Upserting {} gyms".format(len(gyms)))
-        InsertQuery(Gym, rows=gyms.values()).upsert().execute()
+    # if gyms:
+    #     log.info("Upserting {} gyms".format(len(gyms)))
+    #     InsertQuery(Gym, rows=gyms.values()).upsert().execute()
 
 
 def create_tables():
     db.connect()
     db.create_tables([Pokemon, Pokestop, Gym], safe=True)
     db.close()
-
-
-def shurl(longUrl):
-    API_KEY='AIzaSyDIledAeDEV0TytxRu9UkCEILIOlmrhiL0'
-    try:
-        API_KEY
-    except NameError:
-        apiUrl = 'https://www.googleapis.com/urlshortener/v1/url'
-    else:
-        apiUrl = 'https://www.googleapis.com/urlshortener/v1/url?key=%s' % API_KEY
-
-    headers = {"Content-type": "application/json"}
-    data = {"longUrl": longUrl}
-    h = httplib2.Http('.cache')
-    try:
-        headers, response = h.request(apiUrl, "POST", json.dumps(data), headers)
-        short_url = json.loads(response)['id']
-
-    except Exception, e:
-        print "unexpected error %s" % e
-    return short_url
