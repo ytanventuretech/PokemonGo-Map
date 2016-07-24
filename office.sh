9da604ba5574a737438cedc7997297899a53dbab
Ying Tan <ytan@venturetech.net>
2016-07-24 17:13:19 +0000

    update profile.json and office.sh

--- office.sh
+++ office.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-python runserver.py -a google -u ytanpokemon@gmail.com -p pokemonmap -l "40.739467, -96.677618" -st 10
+python runserver.py -a google -u ytanpokemon@gmail.com -p pokemonmap -l "40.739467, -96.677618" -st 10 -pf profile.json
