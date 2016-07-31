8f450f25501b170ab040682de219c951ffb46350
Ying Tan <ytan@venturetech.net>
2016-07-31 12:42:34 -0500

    change the run config

--- office.sh
+++ office.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python runserver.py -a google -u ytanpokemon@gmail.com -p pokemonmap -l "40.739467, -96.677618" -ns -k "AIzaSyCdl-5wDhbzXJknvHsRjdIQg3Irr64dRHQ" -st 10 -pf profile.json
+python runserver.py -a google -u ytanpokemon2@gmail.com -p pokemonMap -l "40.739467, -96.677618" -k "AIzaSyCdl-5wDhbzXJknvHsRjdIQg3Irr64dRHQ" -ns -st 6 -t 1 -sd 2 -pf profile.json
---
+++ skate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python runserver.py -a google -u ytanpokemon2@gmail.com -p pokemonMap -l "40.821441,-96.7177697" -k "AIzaSyCdl-5wDhbzXJknvHsRjdIQg3Irr64dRHQ" -ns -pf profile.json
