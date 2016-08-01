bf4e453820c6e8eee257990527694bc782a16d37
Ying Tan <ytan@venturetech.net>
2016-07-31 21:49:24 -0500

    Add 5 threads.

--- office.sh
+++ office.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python runserver.py -a google -u ytanpokemon@gmail.com -p PokemonMap4321 -l "40.737719,-96.67410378" -k "AIzaSyCdl-5wDhbzXJknvHsRjdIQg3Irr64dRHQ" -st 8 -t 1 -sd 2 -pf profile.json
+python runserver.py -a google -u ytanpokemon@gmail.com -p PokemonMap4321 -u ytanpokemon2@gmail.com -p pokemonMap -u ytanpokemon3@gmail.com -p PokemonMap4321 -u windyxiaopangzi7@gmail.com -p abc654321 -u sunnyxiaopangzi@gmail.com -p abc654321 -l "40.737719,-96.67410378" -k "AIzaSyCdl-5wDhbzXJknvHsRjdIQg3Irr64dRHQ" -st 16 -t 5 -sd 5 -pf profile.json --clear-db
