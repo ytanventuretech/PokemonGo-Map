3df04f9b92bcca21302a1048c54cadf08ebf8fef
Ying Tan <ytan@venturetech.net>
2016-07-31 22:46:52 -0500

    adjust the start location.

--- office.sh
+++ office.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python runserver.py -a google -u ytanpokemon@gmail.com -p PokemonMap4321 -u ytanpokemon2@gmail.com -p pokemonMap -u ytanpokemon3@gmail.com -p PokemonMap4321 -u windyxiaopangzi7@gmail.com -p abc654321 -u sunnyxiaopangzi@gmail.com -p abc654321 -l "40.737719,-96.67410378" -k "AIzaSyCdl-5wDhbzXJknvHsRjdIQg3Irr64dRHQ" -st 16 -t 5 -sd 5 -pf profile.json --clear-db
+python runserver.py -a google -u ytanpokemon@gmail.com -p PokemonMap4321 -u ytanpokemon2@gmail.com -p pokemonMap -u ytanpokemon3@gmail.com -p PokemonMap4321 -u windyxiaopangzi7@gmail.com -p abc654321 -u sunnyxiaopangzi@gmail.com -p abc654321 -l "40.740719, -96.673608" -k "AIzaSyCdl-5wDhbzXJknvHsRjdIQg3Irr64dRHQ" -st 13 -t 5 -sd 5 -pf profile.json --clear-db
